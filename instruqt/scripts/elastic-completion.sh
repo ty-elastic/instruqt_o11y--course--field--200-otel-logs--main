959d629fd5fcc30d6a46df25270aa277ab35af38
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2026-05-26 15:22:45 -0500

    ty

--- instruqt/scripts/elastic-completion.sh
+++ instruqt/scripts/elastic-completion.sh
@@ -8,51 +8,51 @@ else
     echo "LLM_MODEL_ID=$LLM_MODEL_ID"
 fi
 
-/opt/workshops/elastic-llm.sh -m $LLM_MODEL_ID -k true -d true -p false 
+/opt/workshops/elastic-llm.sh -m claude-sonnet-4 -k true -d true
 
 # -------------
 
-echo "Initializing AI Assistant Everywhere"
-init_ai_everywhere() {
-    local http_status=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$KIBANA_URL/internal/kibana/settings" \
-    --header 'Content-Type: application/json' \
-    --header "kbn-xsrf: true" \
-    --header "Authorization: Basic $ELASTICSEARCH_AUTH_BASE64" \
-    --header 'x-elastic-internal-origin: Kibana' \
-    -d '{"changes":{"aiAssistant:preferredAIAssistantType":"observability"}}')
+# echo "Initializing AI Assistant Everywhere"
+# init_ai_everywhere() {
+#     local http_status=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$KIBANA_URL/internal/kibana/settings" \
+#     --header 'Content-Type: application/json' \
+#     --header "kbn-xsrf: true" \
+#     --header "Authorization: Basic $ELASTICSEARCH_AUTH_BASE64" \
+#     --header 'x-elastic-internal-origin: Kibana' \
+#     -d '{"changes":{"aiAssistant:preferredAIAssistantType":"observability"}}')
 
-    if echo $http_status | grep -q '^2'; then
-        echo "Elastic AI Assistant Everywhere successfully initialized: $http_status"
-        return 0
-    else
-        echo "Failed to initialize Elastic AI Assistant Everywhere. HTTP status: $http_status"
-        return 1
-    fi
-}
-retry_command_lin init_ai_everywhere
+#     if echo $http_status | grep -q '^2'; then
+#         echo "Elastic AI Assistant Everywhere successfully initialized: $http_status"
+#         return 0
+#     else
+#         echo "Failed to initialize Elastic AI Assistant Everywhere. HTTP status: $http_status"
+#         return 1
+#     fi
+# }
+# retry_command_lin init_ai_everywhere
 
-# -------------
+# # -------------
 
-echo "Adding completion connector"
-add_connector() {
-    local http_status=$(curl -s -o /dev/null -w "%{http_code}" -X PUT "$ELASTICSEARCH_URL/_inference/completion/openai_completion" \
-    --header 'Content-Type: application/json' \
-    --header "Authorization: Basic $ELASTICSEARCH_AUTH_BASE64" -d'
-    {
-        "service": "openai",
-        "service_settings": {
-            "model_id": "'"$LLM_MODEL_ID"'",
-            "api_key": "'"$LLM_APIKEY"'",
-            "url": "https://'"$LLM_PROXY_URL"'/v1/chat/completions"
-        }
-    }')
+# echo "Adding completion connector"
+# add_connector() {
+#     local http_status=$(curl -s -o /dev/null -w "%{http_code}" -X PUT "$ELASTICSEARCH_URL/_inference/completion/openai_completion" \
+#     --header 'Content-Type: application/json' \
+#     --header "Authorization: Basic $ELASTICSEARCH_AUTH_BASE64" -d'
+#     {
+#         "service": "openai",
+#         "service_settings": {
+#             "model_id": "'"$LLM_MODEL_ID"'",
+#             "api_key": "'"$LLM_APIKEY"'",
+#             "url": "https://'"$LLM_PROXY_URL"'/v1/chat/completions"
+#         }
+#     }')
 
-    if echo $http_status | grep -q '^2'; then
-        echo "Connector added successfully with HTTP status: $http_status"
-        return 0
-    else
-        echo "Failed to add connector. HTTP status: $http_status"
-        return 1
-    fi
-}
-retry_command_lin add_connector
+#     if echo $http_status | grep -q '^2'; then
+#         echo "Connector added successfully with HTTP status: $http_status"
+#         return 0
+#     else
+#         echo "Failed to add connector. HTTP status: $http_status"
+#         return 1
+#     fi
+# }
+# retry_command_lin add_connector
